92799fd1cea98551820a8a98796d281910012d99
root <root@vmi2448259.contaboserver.net>
2025-04-03 03:20:58 -0600

    Agregar archivo setup.py para la instalación del módulo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='facturas_cfdi',
+    version='0.0.1',
+    description='Módulo para administrar e importar facturas CFDI en ERPNext',
+    author='Jose Chavez',
+    author_email='ppchamx@hotmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
